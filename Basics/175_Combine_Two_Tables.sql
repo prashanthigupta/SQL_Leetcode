4ccb3ab1c9e8d2f328c0fe9f11b26395dbca7d0e
Cici Chen <51849709+iamcici0424@users.noreply.github.com>
2021-03-29 20:16:14 -0400

    175

--- Basics/175_Combine_Two_Tables.sql
+++ Basics/175_Combine_Two_Tables.sql
@@ -1,30 +1,30 @@
--- Table: Person
+Table: Person
 
--- +-------------+---------+
--- | Column Name | Type    |
--- +-------------+---------+
--- | PersonId    | int     |
--- | FirstName   | varchar |
--- | LastName    | varchar |
--- +-------------+---------+
--- PersonId is the primary key column for this table.
--- Table: Address
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| PersonId    | int     |
+| FirstName   | varchar |
+| LastName    | varchar |
++-------------+---------+
+PersonId is the primary key column for this table.
+Table: Address
 
--- +-------------+---------+
--- | Column Name | Type    |
--- +-------------+---------+
--- | AddressId   | int     |
--- | PersonId    | int     |
--- | City        | varchar |
--- | State       | varchar |
--- +-------------+---------+
--- AddressId is the primary key column for this table.
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| AddressId   | int     |
+| PersonId    | int     |
+| City        | varchar |
+| State       | varchar |
++-------------+---------+
+AddressId is the primary key column for this table.
  
 
--- Write a SQL query for a report that provides the following information for each person in the Person table,
--- regardless if there is an address for each of those people:
+Write a SQL query for a report that provides the following information for each person in the Person table,
+regardless if there is an address for each of those people:
 
--- FirstName, LastName, City, State
+FirstName, LastName, City, State
 
 
 -- Solution
