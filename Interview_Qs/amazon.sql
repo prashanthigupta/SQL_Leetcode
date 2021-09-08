dc731c12eef634b3ab68b98b14e2b7da42c5c88b
Cici Chen <51849709+iamcici0424@users.noreply.github.com>
2021-09-07 22:13:11 -0400

    1

--- Interview_Qs/amazon.sql
+++ Interview_Qs/amazon.sql
@@ -1,3 +1,5 @@
+
+==========================================================================================
 Order_History table has two columns: 
 CustomerID(varchar), Purchase_Date(date). 
 it has order information for all customers since 2010.
@@ -16,19 +18,21 @@ select
 
 from Order_History
 where year(Purchase_Date) between 2010 and 2015
-group by customerId, year(Purchase_Date))
-
+group by customerId, year(Purchase_Date)
+)
 
 group by customer_id
 having count(y)=5
 
-
-1,2010,xx
-1,2011,
-2,2010,
+1, 2010,xx
+1, 2011,
+2, 2010,
 2, 2011,
 2, 2012,
 2, 2013
 2, 2014
 2, 2015
+=========================================================================================
+
+
 
